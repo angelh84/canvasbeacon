50cfcf2c196196574b8fe9b0b24ea0d4eb8bda94
Angel Hernandez <locus104@yahoo.com>
2017-04-27 23:02:01 -0700

    finished landing page

--- index.php
+++ index.php
@@ -31,8 +31,9 @@
           <div class="col-sm-6">
             <p class="logo">CanvasBeacon.com</p>
             <h1>Declare your sense of style and personality by decorating your space with the finest collection of art.</h1>
+            <img class="world-map visible-xs-block" src="/img/world-map.jpg" alt="World Map Art">
             <p>We tirelessly searched the internet for the best assortment of art and our quest revealed that Society6 carried one of the most original canvas art and prints that we have ever seen.</p>
-            <p>Time to fill those empty walls with something you love and to help with just that, our editors have picked the <span>top 10 most exceptional pieces below</span>.</p>
+            <p>To help you fill those empty walls our editors have picked the <span>top 10 most exceptional art pieces below</span>.</p>
           </div>
         </div>
       </div>
@@ -40,43 +41,243 @@
 
     <section class="body-content clearfix">
       <div class="container">
-        <div class="col-md-6">
-          <div class="art-box">
-            <h2>Alice's Adventures In Wonderland</h2>
-            <a class="author">by Jazzberry Blue</a>
-            <img src="/img/alices-adventures-in-wonderland.jpg" alt="Alice's Adventures In Wonderland">
-            <div class="row">
-              <div class="col-sm-5 col-sm-push-7">
-                <a href="#" class="cta-button">View Profile</a>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="art-box vertical">
+              <div class="title-container clearfix">
+                <p class="position">1</p>
+                <div class="title-box">
+                  <h2>Alice's Adventures In Wonderland</h2>
+                  <a class="author">by Jazzberry Blue</a>  
+                </div>  
               </div>
-              <div class="col-sm-7 col-sm-pull-5">
-                <a href="">surrealism</a>
-                <a href="">abstract</a>
-                <a href="">wonderland</a>
+              <img class="art-image" src="/img/alices-adventures-in-wonderland.jpg" alt="Alice's Adventures In Wonderland">
+              <div class="row">
+                <div class="col-sm-5 col-sm-push-7">
+                  <a href="#" class="cta-button">View Profile</a>
+                </div>
+                <div class="col-sm-7 col-sm-pull-5 categories">
+                  <a href="#">surrealism</a>
+                  <a href="#">abstract</a>
+                  <a href="#">wonderland</a>
+                </div>
               </div>
-              
             </div>
           </div>
-        </div>
-        <div class="col-md-6">
-          <div class="art-box">
-            <h2>Alice's Adventures In Wonderland</h2>
-            <a class="author">by Jazzberry Blue</a>
-            <img src="/img/alices-adventures-in-wonderland.jpg" alt="Alice's Adventures In Wonderland">
-            <div class="row">
-              <div class="col-sm-5 col-sm-push-7">
-                <a href="#" class="cta-button">View Profile</a>
+          <div class="col-md-6">
+            <div class="art-box horizontal">
+              <div class="title-container clearfix">
+                <p class="position">2</p>
+                <div class="title-box">
+                  <h2>Pop Art Feathers</h2>
+                  <a class="author">by Bri.buckley</a>  
+                </div>  
               </div>
-              <div class="col-sm-7 col-sm-pull-5">
-                <a href="">surrealism</a>
-                <a href="">abstract</a>
-                <a href="">wonderland</a>
+              <img class="art-image" src="/img/pop-art-feathers.jpg" alt="Feathers">
+              <div class="row">
+                <div class="col-sm-5 col-sm-push-7">
+                  <a href="#" class="cta-button">View Profile</a>
+                </div>
+                <div class="col-sm-7 col-sm-pull-5 categories">
+                  <a href="#">surrealism</a>
+                  <a href="#">abstract</a>
+                  <a href="#">wonderland</a>
+                </div>
               </div>
-              
             </div>
           </div>
-        </div>
-      </div>
+        </div> <!-- .row -->
+
+        <div class="row">
+          <div class="col-md-6">
+            <div class="art-box horizontal">
+              <div class="title-container clearfix">
+                <p class="position">3</p>
+                <div class="title-box">
+                  <h2>The Parking Ticket</h2>
+                  <a class="author">by Scott Listfield</a>  
+                </div>  
+              </div>
+              <img class="art-image" src="/img/the-parking-ticket.jpg" alt="The Parking Ticket">
+              <div class="row">
+                <div class="col-sm-5 col-sm-push-7">
+                  <a href="#" class="cta-button">View Profile</a>
+                </div>
+                <div class="col-sm-7 col-sm-pull-5 categories">
+                  <a href="#">surrealism</a>
+                  <a href="#">abstract</a>
+                  <a href="#">wonderland</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="art-box vertical">
+              <div class="title-container clearfix">
+                <p class="position">4</p>
+                <div class="title-box">
+                  <h2>Dissolve Me</h2>
+                  <a class="author">by Daniel Taylor</a>  
+                </div>  
+              </div>
+              <img class="art-image" src="/img/dissolve-me.jpg" alt="Dissolve Me">
+              <div class="row">
+                <div class="col-sm-5 col-sm-push-7">
+                  <a href="#" class="cta-button">View Profile</a>
+                </div>
+                <div class="col-sm-7 col-sm-pull-5 categories">
+                  <a href="#">surrealism</a>
+                  <a href="#">abstract</a>
+                  <a href="#">wonderland</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div> <!-- .row -->
+
+        <div class="row">
+          <div class="col-md-6">
+            <div class="art-box horizontal">
+              <div class="title-container clearfix">
+                <p class="position">5</p>
+                <div class="title-box">
+                  <h2>Grand Avenue</h2>
+                  <a class="author">by Tim Jarosz</a>  
+                </div>  
+              </div>
+              <img class="art-image" src="/img/grand-avenue.jpg" alt="Grand Avenue">
+              <div class="row">
+                <div class="col-sm-5 col-sm-push-7">
+                  <a href="#" class="cta-button">View Profile</a>
+                </div>
+                <div class="col-sm-7 col-sm-pull-5 categories">
+                  <a href="#">surrealism</a>
+                  <a href="#">abstract</a>
+                  <a href="#">wonderland</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="art-box horizontal">
+              <div class="title-container clearfix">
+                <p class="position">6</p>
+                <div class="title-box">
+                  <h2>Zebra</h2>
+                  <a class="author">by Jazzberry Blue</a>  
+                </div>  
+              </div>
+              <img class="art-image" src="/img/zebra.jpg" alt="Zebra">
+              <div class="row">
+                <div class="col-sm-5 col-sm-push-7">
+                  <a href="#" class="cta-button">View Profile</a>
+                </div>
+                <div class="col-sm-7 col-sm-pull-5 categories">
+                  <a href="#">surrealism</a>
+                  <a href="#">abstract</a>
+                  <a href="#">wonderland</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div> <!-- .row -->
+
+        <div class="row">
+          <div class="col-md-6">
+            <div class="art-box horizontal">
+              <div class="title-container clearfix">
+                <p class="position">7</p>
+                <div class="title-box">
+                  <h2>Rhythm Of Grief</h2>
+                  <a class="author">by Karl James Mountford</a>  
+                </div>  
+              </div>
+              <img class="art-image" src="/img/rhythm-of-grief.jpg" alt="Rhythm Of Grief">
+              <div class="row">
+                <div class="col-sm-5 col-sm-push-7">
+                  <a href="#" class="cta-button">View Profile</a>
+                </div>
+                <div class="col-sm-7 col-sm-pull-5 categories">
+                  <a href="#">surrealism</a>
+                  <a href="#">abstract</a>
+                  <a href="#">wonderland</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="art-box vertical">
+              <div class="title-container clearfix">
+                <p class="position">8</p>
+                <div class="title-box">
+                  <h2>Archilles</h2>
+                  <a class="author">by Marc Allante</a>  
+                </div>  
+              </div>
+              <img class="art-image" src="/img/archilles.jpg" alt="Archilles">
+              <div class="row">
+                <div class="col-sm-5 col-sm-push-7">
+                  <a href="#" class="cta-button">View Profile</a>
+                </div>
+                <div class="col-sm-7 col-sm-pull-5 categories">
+                  <a href="#">surrealism</a>
+                  <a href="#">abstract</a>
+                  <a href="#">wonderland</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div> <!-- .row -->
+
+        <div class="row">
+          <div class="col-md-6">
+            <div class="art-box vertical">
+              <div class="title-container clearfix">
+                <p class="position">9</p>
+                <div class="title-box">
+                  <h2>Without Words</h2>
+                  <a class="author">by RubbishMonkey</a>  
+                </div>  
+              </div>
+              <img class="art-image" src="/img/without-words.jpg" alt="Without Words">
+              <div class="row">
+                <div class="col-sm-5 col-sm-push-7">
+                  <a href="#" class="cta-button">View Profile</a>
+                </div>
+                <div class="col-sm-7 col-sm-pull-5 categories">
+                  <a href="#">surrealism</a>
+                  <a href="#">abstract</a>
+                  <a href="#">wonderland</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="art-box horizontal">
+              <div class="title-container clearfix">
+                <p class="position">10</p>
+                <div class="title-box">
+                  <h2>Urban Sunset</h2>
+                  <a class="author">by SensualPatterns</a>  
+                </div>  
+              </div>
+              <img class="art-image" src="/img/urban-sunset.jpg" alt="Urban Sunset">
+              <div class="row">
+                <div class="col-sm-5 col-sm-push-7">
+                  <a href="#" class="cta-button">View Profile</a>
+                </div>
+                <div class="col-sm-7 col-sm-pull-5 categories">
+                  <a href="#">surrealism</a>
+                  <a href="#">abstract</a>
+                  <a href="#">wonderland</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div> <!-- .row -->
+
+
+      </div> <!-- .container -->
     </section>
     
   </body>
