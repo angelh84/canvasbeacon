30b22eeda4c835d236d2f85e281f1fef9945b131
Angel Hernandez <locus104@yahoo.com>
2017-05-03 20:31:35 -0700

    added new pixel

--- index.php
+++ index.php
@@ -375,22 +375,22 @@
 
 <!-- Facebook Pixel Code -->
 <script>
-  !function(f,b,e,v,n,t,s)
-  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-  n.queue=[];t=b.createElement(e);t.async=!0;
-  t.src=v;s=b.getElementsByTagName(e)[0];
-  s.parentNode.insertBefore(t,s)}(window,document,'script',
-  'https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '297589584004773'); 
-    fbq('track', 'PageView');
-    $('a').on('click', function() {
-      var target = $(this).parents('.art-box').find('h2').text().replace(/ /g, '').replace(/'/g, '');
-      fbq('trackCustom', 'canvasClickThrough', {custom_param: target});
-    });
-
+!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+document,'script','https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '297589584004773'); // Insert your pixel ID here.
+fbq('track', 'PageView');
+$('a').on('click', function() {
+  fbq('track', 'Lead');  
+});
 </script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=297589584004773&ev=PageView&noscript=1"
+/></noscript>
+<!-- DO NOT MODIFY -->
+<!-- End Facebook Pixel Code -->
 <noscript>
   <img height="1" width="1" src="https://www.facebook.com/tr?id=297589584004773&ev=PageView&noscript=1"/>
 </noscript>
